acd7296821aa241ba5d474cc7aae55dc93329622
yeanjae0312 <yeanjae@naver.com>
2022-05-21 19:37:39 +0900

    Init project set and basic

---
+++ test/test.js
@@ -0,0 +1,8 @@
+"use strict";
+let a;
+// a = 'Mark'
+a = 39;
+function hello(b) {
+    console.log(b);
+}
+hello(39);
---
+++ test/test.ts
@@ -0,0 +1,9 @@
+let a: number
+// a = 'Mark'
+a = 39
+
+function hello(b: number) {
+  console.log(b);
+}
+
+hello(39)
