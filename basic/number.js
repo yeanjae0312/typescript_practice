8dd42d15c0df8ea396f180f195c12f93ac742d01
yeanjae0312 <yeanjae@naver.com>
2022-06-26 20:34:48 +0900

    Add basic folder & 기본형타입 공부
    boolean, number, string타입 공부

---
+++ basic/number.js
@@ -0,0 +1,7 @@
+"use strict";
+let decimal = 6;
+let hex = 0xf00d; // 16진수
+let binary = 0b1010; // 2진수
+let octal = 0o744; // 8진수
+let notANumber = NaN; // NaN은 숫자의 형태
+let underscoreNum = 1000000; // 언더바 지원도 됨
---
+++ basic/number.ts
@@ -0,0 +1,11 @@
+let decimal: number = 6;
+
+let hex: number = 0xf00d; // 16진수
+
+let binary: number = 0b1010; // 2진수
+
+let octal: number = 0o744; // 8진수
+
+let notANumber: number = NaN; // NaN은 숫자의 형태
+
+let underscoreNum: number = 1_000_000; // 언더바 지원도 됨
